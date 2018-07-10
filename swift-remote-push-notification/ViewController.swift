7c9c1ba7708e7f573dae795602b71b98236f0401
Sanjay Pandit <sanjuraj85@gmail.com>
2018-07-10 18:22:39 +0530

    Initial Commit

---
+++ swift-remote-push-notification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-remote-push-notification
+//
+//  Created by Pandit, Sanjay on 7/10/18.
+//  Copyright © 2018 Pandit, Sanjay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
